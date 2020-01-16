6b6344d2a717d75f10a66a4e9a28de80b68af2aa
Pedro Henrique <38913462+PH7-Jack@users.noreply.github.com>
2020-01-16 14:20:15 -0300

    Create WebHelpers.php

---
+++ Helpers/WebHelpers.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Helpers;
+
+use Illuminate\Http\Request;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+
+class WebHelpers
+{
+    public static function getUrlResponse($url)
+    {
+        try {
+            $client = new Client();
+            $response = $client->get($url, ['http_errors' => false]);
+            $body = $response->getBody()->getContents();
+            return json_decode($body, true);
+        } catch (RequestException $ex) {
+            return $ex->getMessage();
+        }
+    }
+}
